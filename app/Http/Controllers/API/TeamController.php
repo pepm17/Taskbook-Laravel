71ba3728fb5490414d676e6d98e5b8bb6166285e
Pablo Emilio <pablopupo1797@gmail.com>
2019-02-23 17:32:51 -0500

    Crud Team
    Se agregaron los metodos crud al controlador team.
    Se creó un controlador, que se encarga de enviar los mensajes de la api.
    Se agregó la ruta.

--- app/Http/Controllers/API/TeamController.php
+++ app/Http/Controllers/API/TeamController.php
@@ -3,26 +3,51 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Team;
+use Validator;
 
-class TeamController extends Controller{
+class TeamController extends BaseController{
     public function index(){
-        //
+        $team = Team::all();
+        return $this->sendResponse($team->toArray(), 'Team retrieved succesfully.');
     }
 
     public function store(Request $request){
-        //
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+        if($validator->fails()) return $this->sendError('Validation error.', $validator->errors());
+        $team = Team::create($input);
+        $team->creator = auth()->user()->id;
+        $team->save();
+        return $this->sendResponse($team->toArray(), 'Team created successfully.');
     }
 
     public function show($id){
-        //
+        $team = Team::find($id);
+        if(is_null($team)) return $this->sendError('Team not found');
+        return $this->sendResponse($team->toArray(), 'Team retrieved successfully.');
     }
 
     public function update(Request $request, $id){
-        //
+        $team = Team::find($id);
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+        if($validator->fails()) return $this->sendError('Validation Error.', $validator->errors());
+        $team->name = $input['name'];
+        $team->description = $input['description'];
+        $team->save();
+        return $this->sendResponse($team->toArray(), 'Team updated succesfully.');
     }
 
     public function destroy($id){
-        //
+        $team = Team::destroy($id);
+        return $this->sendResponse($team->toArray(), 'Team deleted succesfully.');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
 });
-//Route::apiResources('team', 'TeamController');
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-  //  return $request->user();
-//});
+//Route::apiResources('team', 'API\TeamController');
+Route::middleware('auth:api')->group(function () {
+    Route:resource('team', 'API\TeamController');
+});
