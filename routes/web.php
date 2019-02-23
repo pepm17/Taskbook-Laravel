75f66d5aba2cb299a57f48327df3ec9413dacdc3
Pablo Emilio <pablopupo1797@gmail.com>
2019-02-23 10:40:18 -0500

    Instalación de Passport
    Sistema de autentificación para poder usar el sistema de autentificación como Api

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource([
+/*Route::resource([
     'team' => 'TeamController',
     'activity' => 'ActivityController',
     'response' => 'ResponseController'
-]);
+]);*/
