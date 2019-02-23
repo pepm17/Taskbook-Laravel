210532105959628be2ad49e275a7fb4815fede13
Pablo Emilio <pablopupo1797@gmail.com>
2019-02-23 15:59:03 -0500

    Cambios
    Se cambiaron de carpeta los controladores

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
 });
+//Route::apiResources('team', 'TeamController');
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
   //  return $request->user();
 //});
